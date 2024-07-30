b280a40e82c31f600891ec52e0a8fdbe68fefc4a
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-07-30 16:54:42 +0200

    [UPD]PRINT API RESPONSE IN CONSOLE

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import { store } from "./store";
+export default {
+  data() {
+    return {
+      store,
+      projectList: [],
+    };
+  },
+  created() {
+    console.log("chiamata api");
+
+    axios.get(store.APIurl).then((response) => {
+      console.log(response);
+      this.store.ApiCall = response.data;
+      this.store.projectList = response.data.results;
+      this.projectList = store.projectList;
+    });
+  },
+};
+</script>
+
+<template>
+  list
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
